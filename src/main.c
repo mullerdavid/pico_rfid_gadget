bbd8d1515e28d29a96c2e02dd328f2e9ec5837d4
Deathbaron <user@deathbaron.org>
2023-04-03 20:10:20 +0200

    bugfix for menu length, dropping example code

--- src/main.c
+++ src/main.c
@@ -57,7 +57,7 @@ void draw_menu(size_t argc, const char** argv, size_t selected, size_t* start)
     x+=spacing;
 
     uint8_t i;
-    for (i=*start;i<((*start)+maxnum);i++)
+    for (i=*start;i<((*start)+maxnum) && i<argc;i++)
     {
         y+=spacing;
         const char* current = argv[i];
@@ -89,7 +89,6 @@ void SET_Button_PIN(uint8_t PIN)
 int main_core0() 
 {
     /* Buttons */
-    
     uint8_t keyA = 15; 
     uint8_t keyB = 17; 
     uint8_t keyX = 19; 
@@ -129,64 +128,11 @@ int main_core0()
     Paint_Clear(WHITE);
     
     // /* GUI */
-    printf("drawing...\r\n");
-    
-#if 0
-     Paint_DrawImage(gImage_1inch3_1,0,0,240,240);
-     LCD_1IN3_Display(FrameBuffer);
-     DEV_Delay_ms(2000);
-#endif
-#if 0
-    Paint_DrawPoint(2,1, BLACK, DOT_PIXEL_1X1,  DOT_FILL_RIGHTUP);//240 240
-    Paint_DrawPoint(2,6, BLACK, DOT_PIXEL_2X2,  DOT_FILL_RIGHTUP);
-    Paint_DrawPoint(2,11, BLACK, DOT_PIXEL_3X3, DOT_FILL_RIGHTUP);
-    Paint_DrawPoint(2,16, BLACK, DOT_PIXEL_4X4, DOT_FILL_RIGHTUP);
-    Paint_DrawPoint(2,21, BLACK, DOT_PIXEL_5X5, DOT_FILL_RIGHTUP);
-    Paint_DrawLine( 10,  5, 40, 35, MAGENTA, DOT_PIXEL_2X2, LINE_STYLE_SOLID);
-    Paint_DrawLine( 10, 35, 40,  5, MAGENTA, DOT_PIXEL_2X2, LINE_STYLE_SOLID);
-
-    Paint_DrawLine( 80,  20, 110, 20, CYAN, DOT_PIXEL_1X1, LINE_STYLE_DOTTED);
-    Paint_DrawLine( 95,   5,  95, 35, CYAN, DOT_PIXEL_1X1, LINE_STYLE_DOTTED);
-
-    Paint_DrawRectangle(10, 5, 40, 35, RED, DOT_PIXEL_2X2,DRAW_FILL_EMPTY);
-    Paint_DrawRectangle(45, 5, 75, 35, BLUE, DOT_PIXEL_2X2,DRAW_FILL_FULL);
-
-    Paint_DrawCircle(95, 20, 15, GREEN, DOT_PIXEL_1X1, DRAW_FILL_EMPTY);
-    Paint_DrawCircle(130, 20, 15, GREEN, DOT_PIXEL_1X1, DRAW_FILL_FULL);
-
-
-    Paint_DrawNum (50, 40 ,9.87654321, &Font20,3,  WHITE,  BLACK);
-    Paint_DrawString_EN(1, 40, "ABC", &Font20, 0x000f, 0xfff0);
-    Paint_DrawString_EN(1, 100, "WaveShare", &Font16, RED, WHITE); 
-    Paint_DrawString_EN(1, 120, "0123456789", &Font24, WHITE, BLACK);
-    Paint_DrawString_EN(1, 150, "1111111111", &Font24, WHITE, BLACK);
-    Paint_DrawString_EN(1, 180, "0000000000", &Font24, WHITE, BLACK);
-
-    // /*3.Refresh the picture in RAM to LCD*/
-    LCD_1IN3_Display(FrameBuffer);
-    DEV_Delay_ms(2000);
-
-#endif
-#if 1
-
 
     const char* menu[] = {
         "Menuitem 1",
         "Menuitem 2",
-        "Menuitem 3",
-        "Menuitem 4",
-        "Menuitem 5",
-        "Menuitem 6 this is very looong",
-        "Menuitem 7",
-        "Menuitem 8",
-        "Menuitem 9",
-        "Menuitem 10",
-        "11 Lorem ipsum",
-        "12 dolor sit amet",
-        "13 consectetur",
-        "14 adipiscing elit",
-        "15 sed do eiusmod",
-        "16  tempor incididunt"
+        "Menuitem 3"
     };
 
     const uint32_t repeat_ms = 250;
@@ -244,130 +190,11 @@ int main_core0()
         }
         sleep_ms(15);
     }
-    sleep_ms(1000);
 
-#endif
-#if 1
-
-    Paint_Clear(WHITE);
-    Paint_DrawRectangle(208, 15, 237, 45, 0xF800, DOT_PIXEL_2X2,DRAW_FILL_EMPTY);
-    Paint_DrawRectangle(208, 75, 237, 105, 0xF800, DOT_PIXEL_2X2,DRAW_FILL_EMPTY);
-    Paint_DrawRectangle(208, 135, 237, 165, 0xF800, DOT_PIXEL_2X2,DRAW_FILL_EMPTY);
-    Paint_DrawRectangle(208, 195, 237, 225, 0xF800, DOT_PIXEL_2X2,DRAW_FILL_EMPTY);
-    Paint_DrawRectangle(60, 60, 91, 90, 0xF800, DOT_PIXEL_2X2,DRAW_FILL_EMPTY);
-    Paint_DrawRectangle(60, 150, 91, 180, 0xF800, DOT_PIXEL_2X2,DRAW_FILL_EMPTY);
-    Paint_DrawRectangle(15, 105, 46, 135, 0xF800, DOT_PIXEL_2X2,DRAW_FILL_EMPTY);
-    Paint_DrawRectangle(105, 105, 136, 135, 0xF800, DOT_PIXEL_2X2,DRAW_FILL_EMPTY);
-    Paint_DrawRectangle(60, 105, 91, 135, 0xF800, DOT_PIXEL_2X2,DRAW_FILL_EMPTY);
-    LCD_1IN3_Display(FrameBuffer);
-
-    
-    while(1){
-        if(DEV_Digital_Read(keyA ) == 0){
-            Paint_DrawRectangle(208, 15, 236, 45, 0xF800, DOT_PIXEL_2X2,DRAW_FILL_FULL);
-            LCD_1IN3_DisplayWindows(208, 15, 236, 45,FrameBuffer);
-            //printf("gpio =%d\r\n",keyA);
-        }
-        else{
-            Paint_DrawRectangle(208, 15, 236, 45, WHITE, DOT_PIXEL_2X2,DRAW_FILL_FULL);
-            LCD_1IN3_DisplayWindows(208, 15, 236, 45,FrameBuffer);
-        }
-            
-        if(DEV_Digital_Read(keyB ) == 0){
-            Paint_DrawRectangle(208, 75, 236, 105, 0xF800, DOT_PIXEL_2X2,DRAW_FILL_FULL);
-            LCD_1IN3_DisplayWindows(208, 75, 236, 105,FrameBuffer);
-            //printf("gpio =%d\r\n",keyB);
-        }
-        else{
-            Paint_DrawRectangle(208, 75, 236, 105, WHITE, DOT_PIXEL_2X2,DRAW_FILL_FULL);
-            LCD_1IN3_DisplayWindows(208, 75, 236, 105,FrameBuffer);
-        }
-        
-        if(DEV_Digital_Read(keyX ) == 0){
-            Paint_DrawRectangle(208, 135, 236, 165, 0xF800, DOT_PIXEL_2X2,DRAW_FILL_FULL);
-            LCD_1IN3_DisplayWindows(208, 135, 236, 165,FrameBuffer);
-            //printf("gpio =%d\r\n",keyX);
-        }
-        else{
-            Paint_DrawRectangle(208, 135, 236, 165, WHITE, DOT_PIXEL_2X2,DRAW_FILL_FULL);
-            LCD_1IN3_DisplayWindows(208, 135, 236, 165,FrameBuffer);
-        }
-            
-        if(DEV_Digital_Read(keyY ) == 0){
-            Paint_DrawRectangle(208, 195, 236, 225, 0xF800, DOT_PIXEL_2X2,DRAW_FILL_FULL);
-            LCD_1IN3_DisplayWindows(208, 195, 236, 225,FrameBuffer);
-            //printf("gpio =%d\r\n",keyY);
-        }
-        else{
-            Paint_DrawRectangle(208, 195, 236, 225, WHITE, DOT_PIXEL_2X2,DRAW_FILL_FULL);
-            LCD_1IN3_DisplayWindows(208, 195, 236, 225,FrameBuffer);
-        }
-
-
-        if(DEV_Digital_Read(up ) == 0){
-            Paint_DrawRectangle(60, 60, 90, 90, 0xF800, DOT_PIXEL_2X2,DRAW_FILL_FULL);
-            LCD_1IN3_DisplayWindows(60, 60, 90, 90,FrameBuffer);
-            //printf("gpio =%d\r\n",up);
-        }
-        else{
-            Paint_DrawRectangle(60, 60, 90, 90, WHITE, DOT_PIXEL_2X2,DRAW_FILL_FULL);
-            LCD_1IN3_DisplayWindows(60, 60, 90, 90,FrameBuffer);
-        }
-
-        if(DEV_Digital_Read(down ) == 0){
-            Paint_DrawRectangle(60, 150, 90, 180, 0xF800, DOT_PIXEL_2X2,DRAW_FILL_FULL);
-            LCD_1IN3_DisplayWindows(60, 150, 90, 180,FrameBuffer);
-            //printf("gpio =%d\r\n",dowm);
-        }
-        else{
-            Paint_DrawRectangle(60, 150, 90, 180, WHITE, DOT_PIXEL_2X2,DRAW_FILL_FULL);
-            LCD_1IN3_DisplayWindows(60, 150, 90, 180,FrameBuffer);
-        }
-        
-        if(DEV_Digital_Read(left ) == 0){
-            Paint_DrawRectangle(15, 105, 45, 135, 0xF800, DOT_PIXEL_2X2,DRAW_FILL_FULL);
-            LCD_1IN3_DisplayWindows(15, 105, 45, 135,FrameBuffer);
-            //printf("gpio =%d\r\n",left);
-        }
-        else{
-            Paint_DrawRectangle(15, 105, 45, 135, WHITE, DOT_PIXEL_2X2,DRAW_FILL_FULL);
-            LCD_1IN3_DisplayWindows(15, 105, 45, 135,FrameBuffer);
-        }
-            
-        if(DEV_Digital_Read(right ) == 0){
-            Paint_DrawRectangle(105, 105, 135, 135, 0xF800, DOT_PIXEL_2X2,DRAW_FILL_FULL);
-            LCD_1IN3_DisplayWindows(105, 105, 135, 135,FrameBuffer);
-            //printf("gpio =%d\r\n",right);
-        }
-        else{
-            Paint_DrawRectangle(105, 105, 135, 135, WHITE, DOT_PIXEL_2X2,DRAW_FILL_FULL);
-            LCD_1IN3_DisplayWindows(105, 105, 135, 135,FrameBuffer);
-        }
-        
-        if(DEV_Digital_Read(ctrl ) == 0){
-            Paint_DrawRectangle(60, 105, 90, 135, 0xF800, DOT_PIXEL_2X2,DRAW_FILL_FULL);
-            LCD_1IN3_DisplayWindows(60, 105, 90, 135,FrameBuffer);
-            //printf("gpio =%d\r\n",ctrl);
-        }
-        else{
-            Paint_DrawRectangle(60, 105, 90, 135, WHITE, DOT_PIXEL_2X2,DRAW_FILL_FULL);
-            LCD_1IN3_DisplayWindows(60, 105, 90, 135,FrameBuffer);
-        }
-
-    }
-
-#endif
 
     /* Module Exit */
     free(FrameBuffer);
     FrameBuffer = NULL;
-    
-
-    while(1){
-	    //static int counter = 0;
-        //printf("[core0] At cycle %d\n", counter++);
-        sleep_ms(1000);
-    }
     
     DEV_Module_Exit();
     return 0;
